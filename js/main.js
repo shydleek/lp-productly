324b283bdde0e57e8f7a315f9e4157f770c5e3bf
Matthew <matiksh.2002@gmail.com>
2021-04-20 01:19:47 +0300

    Completed adaptive desktop-first landing page

--- js/main.js
+++ js/main.js
@@ -3,6 +3,27 @@ $(function(){
         arrows: false,
         dots: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1201,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 781,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
     });
+
+    $('.menu__btn').on('click', function(){
+        $('.header__list').toggleClass('header__list--active')
+    });
+
 });
